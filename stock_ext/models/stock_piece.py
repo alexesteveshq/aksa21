f591392daa0b07a9f312b8663c33907ae4f0cbd8
Alex Rafael Esteves Balado <alexrafael.esteves@es.gt.com>
2023-02-13 00:22:40 +0100

    [Alex] agregar refrescamiento de pagina al agregar pieza

--- stock_ext/models/stock_piece.py
+++ stock_ext/models/stock_piece.py
@@ -11,3 +11,13 @@ class StockPiece(models.Model):
     product_id = fields.Many2one('product.product', string='Product')
     weight = fields.Float(string='Weight')
     price = fields.Float(string='Price')
+
+    def create_from_scale(self, weight, lot_id):
+        self.env['stock.piece'].create({'weight': weight, 'lot_id': lot_id})
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload',
+        }
+
+    def print_sticker(self):
+        print("Hola")

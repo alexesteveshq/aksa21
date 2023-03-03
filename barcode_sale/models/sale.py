5e2e0b1495f10e5152e5520dbc51c8fd6247c512
Alex Rafael Esteves Balado <alexrafael.esteves@es.gt.com>
2023-03-03 23:34:55 +0100

    [Alex] correcciones para logica de inventario y venta

--- barcode_sale/models/sale.py
+++ barcode_sale/models/sale.py
@@ -14,8 +14,10 @@ class SaleOrder(models.Model):
     _name = 'sale.order'
     _inherit = ['sale.order', 'barcodes.barcode_events_mixin']
 
-    def on_barcode_scanned(self, barcode):
-        product = self.env['product.product'].search([('barcode', 'like', barcode)], limit=1)
+    def on_barcode_scanned(self, barcode=''):
+        product = self.env['product.product'].search([('barcode', '=', barcode.upper())], limit=1)
+        if product.qty_available == 0:
+            raise UserError(_('Scanned piece with barcode %s is not available.') % barcode)
         if product:
             self.order_line = [(0, 0, {'product_id': product.id,
                                        'product_uom_qty': 1,

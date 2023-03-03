ba96d2455b978bad4ea8e272c074e1f89500a310
Alex Rafael Esteves Balado <alexrafael.esteves@es.gt.com>
2023-03-03 23:46:03 +0100

    [Alex] correcciones para logica de inventario y venta

--- barcode_sale/models/sale.py
+++ barcode_sale/models/sale.py
@@ -21,6 +21,6 @@ class SaleOrder(models.Model):
         if piece:
             self.order_line = [(0, 0, {'product_id': piece.id,
                                        'product_uom_qty': 1,
-                                       'price_unit': piece.price_lst})]
+                                       'price_unit': piece.lst_price})]
         else:
             raise UserError(_('Scanned piece with barcode %s does not exist.') % barcode)

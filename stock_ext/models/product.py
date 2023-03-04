741e883cebeabc2ec07b900dc3c647f5c643cfa1
Alex Rafael Esteves Balado <alexrafael.esteves@es.gt.com>
2023-03-04 18:54:42 +0100

    [Alex] corregir problema al crear productos

--- stock_ext/models/product.py
+++ stock_ext/models/product.py
@@ -3,6 +3,13 @@
 from odoo import fields, models, api
 
 
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    price_usd = fields.Float(string='Sale Price')
+    detailed_type = fields.Selection(default='product')
+
+
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 

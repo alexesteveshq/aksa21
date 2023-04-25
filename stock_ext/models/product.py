14a29fddf0c496709067e5087d58ac76b31f7772
Alex Rafael Esteves Balado <alexrafael.esteves@es.gt.com>
2023-04-26 01:33:56 +0200

    [Alex] corregir problema de codigos de barra en minuscula

--- stock_ext/models/product.py
+++ stock_ext/models/product.py
@@ -14,3 +14,9 @@ class ProductProduct(models.Model):
 
     list_price = fields.Float(string='List price')
     pieces_ids = fields.One2many('stock.piece', 'product_id', string='Pieces')
+
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
+        name = name.upper()
+        return super(ProductProduct, self)._name_search(
+            name=name, args=args, operator=operator, limit=limit, name_get_uid=name_get_uid)

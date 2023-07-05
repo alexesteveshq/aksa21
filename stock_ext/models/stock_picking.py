d64ab58a77bd2cbc8b49c81e86683ef7efdab96d
Alex Rafael Esteves Balado <alexrafael.esteves@es.gt.com>
2023-07-05 22:50:11 +0200

    [Alex] corregir calculo de variante retail

--- stock_ext/models/stock_picking.py
+++ stock_ext/models/stock_picking.py
@@ -8,6 +8,13 @@ class StockPicking(models.Model):
 
     retail_variant = fields.Float(string='Retail variant')
 
+    @api.model_create_multi
+    def create(self, vals):
+        result = super(StockPicking, self).create(vals)
+        for picking in result:
+            picking.move_line_ids.mapped('product_id').write({'retail_variant': picking.retail_variant})
+        return result
+
     def write(self, vals):
         res = super(StockPicking, self).write(vals)
         if vals and 'retail_variant' in vals and vals['retail_variant']:

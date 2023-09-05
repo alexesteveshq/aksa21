d4bdf9b875934faf5658fa6d2f715860b005cf21
Alex Rafael Esteves Balado <alexrafael.esteves@es.gt.com>
2023-09-05 15:35:28 +0200

    [Alex] corregir calculo de reportes

--- pos_ext/models/pos_order_report.py
+++ pos_ext/models/pos_order_report.py
@@ -6,7 +6,7 @@ from odoo import fields, models, _
 class PosOrderReport(models.Model):
     _inherit = 'report.pos.order'
 
-    quantity_average = fields.Float(string='Quantity average', readonly=True, group_operator="avg")
+    quantity_average = fields.Float(string='Quantity average', group_operator="avg")
     discount_average = fields.Float(string='Discount average', readonly=True, group_operator="avg")
     sale_avg = fields.Float(string='Sale average', group_operator="avg")
     cash_total = fields.Float(string='Cash total')
@@ -15,9 +15,15 @@ class PosOrderReport(models.Model):
 
     def _select(self):
         res = super(PosOrderReport, self)._select()
-        res += (', s.sale_qty AS quantity_average, SUM(l.discount) AS discount_average '
+        res += (', SUM(l.qty) AS quantity_average '
+                ', SUM(l.discount) AS discount_average '
                 ', s.cash_paid_amount / s.sale_qty AS cash_total'
                 ', s.bank_paid_amount / s.sale_qty AS bank_total'
-                ', s.amount_paid / s.sale_qty AS sale_total'
-                ', s.amount_paid AS sale_avg')
+                ', SUM(l.price_subtotal_incl) AS sale_total'
+                ', SUM(l.price_subtotal_incl) AS sale_avg')
+        return res
+
+    def _from(self):
+        res = super(PosOrderReport, self)._from()
+        res += """ WHERE l.qty != 0 """
         return res

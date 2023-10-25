6ee9a77c9a879c288e0ba9f68c86f4eb3596854a
Alex Rafael Esteves Balado <alexrafael.esteves@es.gt.com>
2023-10-25 13:15:22 +0200

    [Alex] desglosar iva para costo de mercancia

--- pos_ext/models/pos_session.py
+++ pos_ext/models/pos_session.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models, _
+from odoo.tools import float_is_zero
+from odoo.exceptions import UserError
 
 
 class PosSession(models.Model):
@@ -28,3 +30,43 @@ class PosSession(models.Model):
             if product:
                 return {'product_id': [product.id]}
         return result
+
+    def _create_non_reconciliable_move_lines(self, data):
+        # Create account.move.line records for
+        #   - sales
+        #   - taxes
+        #   - stock expense
+        #   - non-cash split receivables (not for automatic reconciliation)
+        #   - non-cash combine receivables (not for automatic reconciliation)
+        taxes = data.get('taxes')
+        sales = data.get('sales')
+        stock_expense = data.get('stock_expense')
+        rounding_difference = data.get('rounding_difference')
+        MoveLine = data.get('MoveLine')
+
+        tax_vals = [self._get_tax_vals(key, amounts['amount'], amounts['amount_converted'], amounts['base_amount_converted']) for key, amounts in taxes.items()]
+        # Check if all taxes lines have account_id assigned. If not, there are repartition lines of the tax that have no account_id.
+        tax_names_no_account = [line['name'] for line in tax_vals if line['account_id'] == False]
+        if len(tax_names_no_account) > 0:
+            error_message = _(
+                'Unable to close and validate the session.\n'
+                'Please set corresponding tax account in each repartition line of the following taxes: \n%s'
+            ) % ', '.join(tax_names_no_account)
+            raise UserError(error_message)
+        rounding_vals = []
+
+        if not float_is_zero(rounding_difference['amount'], precision_rounding=self.currency_id.rounding) or not float_is_zero(rounding_difference['amount_converted'], precision_rounding=self.currency_id.rounding):
+            rounding_vals = [self._get_rounding_difference_vals(rounding_difference['amount'], rounding_difference['amount_converted'])]
+
+        MoveLine.create(tax_vals)
+        move_line_ids = MoveLine.create([self._get_sale_vals(key, amounts['amount'], amounts['amount_converted']) for key, amounts in sales.items()])
+        for key, ml_id in zip(sales.keys(), move_line_ids.ids):
+            sales[key]['move_line_id'] = ml_id
+        tax_account = self.picking_ids[0].product_id.categ_id.property_account_tax_categ_id
+        tax = self.picking_ids[0].product_id.taxes_id
+        for key, amounts in stock_expense.items():
+            tax_amount = (amounts['amount'] * tax.amount) / 100
+            amount_untaxed = amounts['amount'] - tax_amount
+            MoveLine.create(self._get_stock_expense_vals(key, amount_untaxed, amount_untaxed))
+            MoveLine.create(self._get_stock_expense_vals(tax_account, tax_amount, tax_amount))
+        return data

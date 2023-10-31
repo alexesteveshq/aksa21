d0762a3b511d1a069a7d5e9568912e8a87ebc786
Alex Rafael Esteves Balado <alexrafael.esteves@es.gt.com>
2023-11-01 00:29:35 +0100

    [Alex] unificar costos

--- stock_ext/wizard/stock_move_transfer_wizard.py
+++ stock_ext/wizard/stock_move_transfer_wizard.py
@@ -12,16 +12,18 @@ class StockMoveTransfer(models.TransientModel):
     location_dest_id = fields.Many2one('stock.location', string='Destination location')
 
     def create_transfer(self):
-        moves = self.env['stock.move'].browse(self._context.get('active_ids'))
-        if moves:
-            self.env['stock.picking'].create({
-                'partner_id': self.partner_id.id,
-                'location_id': self.location_id.id,
-                'location_dest_id': self.location_dest_id.id,
-                'picking_type_id': self.env.ref('stock.picking_type_internal').id,
-                'move_ids_without_package':
-                    [(0, 0, {'product_id': move.product_id.id,
-                             'name': 'interco transfer',
-                             'location_id': self.location_id.id,
-                             'location_dest_id': self.location_dest_id.id,
-                             'product_uom_qty': 1}) for move in moves]})
+        quants = self.env['stock.quant'].sudo().browse(self._context.get('active_ids'))
+        picking_type = self.env['stock.picking.type'].search([('sequence_code', '=', 'INT')])
+        if quants and picking_type:
+            transfer = self.env['stock.picking'].sudo().create({
+                    'partner_id': self.partner_id.id,
+                    'location_id': self.location_id.id,
+                    'location_dest_id': self.location_dest_id.id,
+                    'picking_type_id': picking_type.id,
+                    'move_ids_without_package':
+                        [(0, 0, {'product_id': quant.product_id.id,
+                                 'name': 'interco transfer',
+                                 'location_id': self.location_id.id,
+                                 'location_dest_id': self.location_dest_id.id,
+                                 'product_uom_qty': 1}) for quant in quants]})
+            transfer.company_id = self.env.company.id

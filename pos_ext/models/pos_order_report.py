17acb3b6845e0a43548a89f3fd9b32fd0d6d1f70
Alex Rafael Esteves Balado <alexrafael.esteves@es.gt.com>
2023-11-18 01:05:18 +0100

    [Alex] corregir ventas totales en reportes

--- pos_ext/models/pos_order_report.py
+++ pos_ext/models/pos_order_report.py
@@ -11,7 +11,6 @@ class PosOrderReport(models.Model):
     sale_avg = fields.Float(string='Sale average', group_operator="avg")
     cash_total = fields.Float(string='Cash total')
     bank_total = fields.Float(string='Bank total')
-    sale_total = fields.Float(string='Sale total')
 
     @api.model
     def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
@@ -26,14 +25,41 @@ class PosOrderReport(models.Model):
         return result
 
     def _select(self):
-        res = super(PosOrderReport, self)._select()
-        res += (', SUM(l.qty) AS quantity_average '
-                ', AVG(l.discount) FILTER (WHERE l.discount > 0) AS discount_average '
-                ', s.cash_paid_amount / s.sale_qty AS cash_total'
-                ', s.bank_paid_amount / s.sale_qty AS bank_total'
-                ', SUM(l.price_subtotal_incl) AS sale_total'
-                ', SUM(l.price_subtotal_incl) AS sale_avg')
-        return res
+        return """
+            SELECT
+                MIN(l.id) AS id,
+                COUNT(*) AS nbr_lines,
+                s.date_order AS date,
+                SUM(l.qty) AS product_qty,
+                SUM(l.qty * l.price_unit / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS price_sub_total,
+                SUM((l.qty * l.price_unit) * (l.discount / 100) / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS total_discount,
+                CASE
+                    WHEN SUM(l.qty * u.factor) = 0 THEN NULL
+                    ELSE (SUM(l.qty*l.price_unit / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END)/SUM(l.qty * u.factor))::decimal
+                END AS average_price,
+                SUM(cast(to_char(date_trunc('day',s.date_order) - date_trunc('day',s.create_date),'DD') AS INT)) AS delay_validation,
+                s.id as order_id,
+                s.partner_id AS partner_id,
+                s.state AS state,
+                s.user_id AS user_id,
+                s.company_id AS company_id,
+                s.sale_journal AS journal_id,
+                l.product_id AS product_id,
+                pt.categ_id AS product_categ_id,
+                p.product_tmpl_id,
+                ps.config_id,
+                pt.pos_categ_id,
+                s.pricelist_id,
+                s.session_id,
+                s.account_move IS NOT NULL AS invoiced,
+                SUM(l.price_subtotal - COALESCE(l.total_cost,0) / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS margin,
+                SUM(l.qty) AS quantity_average,
+                AVG(l.discount) FILTER (WHERE l.discount > 0) AS discount_average,
+                s.cash_paid_amount / s.sale_qty AS cash_total,
+                s.bank_paid_amount / s.sale_qty AS bank_total,
+                SUM(l.price_subtotal_incl) AS price_total,
+                SUM(l.price_subtotal_incl) AS sale_avg
+        """
 
     def _from(self):
         res = super(PosOrderReport, self)._from()

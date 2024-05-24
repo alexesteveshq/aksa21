f498c335bd1856c3dbe20a3e9b8214bf6fdb19b0
Alex <alexesteveshq@gmail.com>
2024-05-24 21:51:49 +0200

    [Alex] agregar cost de venta a las ordenes

--- pos_ext/models/pos_order_report.py
+++ pos_ext/models/pos_order_report.py
@@ -26,41 +26,12 @@ class PosOrderReport(models.Model):
         return result
 
     def _select(self):
-        return """
-            SELECT
-                MIN(l.id) AS id,
-                COUNT(*) AS nbr_lines,
-                s.date_order AS date,
-                SUM(l.qty) AS product_qty,
-                SUM(l.qty * l.price_unit / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS price_sub_total,
-                SUM((l.qty * l.price_unit) * (l.discount / 100) / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS total_discount,
-                CASE
-                    WHEN SUM(l.qty * u.factor) = 0 THEN NULL
-                    ELSE (SUM(l.qty*l.price_unit / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END)/SUM(l.qty * u.factor))::decimal
-                END AS average_price,
-                SUM(cast(to_char(date_trunc('day',s.date_order) - date_trunc('day',s.create_date),'DD') AS INT)) AS delay_validation,
-                s.id as order_id,
-                s.partner_id AS partner_id,
-                s.state AS state,
-                s.user_id AS user_id,
-                s.company_id AS company_id,
-                s.sale_journal AS journal_id,
-                l.product_id AS product_id,
-                pt.categ_id AS product_categ_id,
-                p.product_tmpl_id,
-                ps.config_id,
-                s.seller_id AS seller_id,
-                pt.pos_categ_id,
-                s.pricelist_id,
-                s.session_id,
-                s.account_move IS NOT NULL AS invoiced,
-                SUM(l.price_subtotal - COALESCE(l.total_cost,0) / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS margin,
-                SUM(l.qty) AS quantity_average,
+        result = super(PosOrderReport, self)._select()
+        result += """, SUM(l.qty) AS quantity_average,
                 AVG(l.discount) FILTER (WHERE l.discount > 0) AS discount_average,
-                SUM(l.price_subtotal_incl) AS price_total,
                 SUM(l.amount_currency) AS price_total_currency,
-                SUM(l.price_subtotal_incl) AS sale_avg
-        """
+                SUM(l.price_subtotal_incl) AS sale_avg"""
+        return result
 
     def _from(self):
         res = super(PosOrderReport, self)._from()
---
+++ pos_product_cost/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'POS Product cost',
+    'summary': 'Adds product cost in point of sale order and lines.'
+               'POS product cost|Point of sale product cost|Sold products cost|Sold product cost|'
+               'POS cost|Point of sale cost',
+    'description': 'Adds product cost in point of sale order and lines',
+    'category': 'Point of sale',
+    'author': 'Visionee',
+    'version': '16.0.1.0',
+    'depends': [
+        'point_of_sale',
+    ],
+    'data': [
+        'views/pos_order_views.xml',
+        'views/pos_order_report_view.xml',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+}

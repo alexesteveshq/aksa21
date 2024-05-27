135d5addc3b3d8ce5815f21b07c35d56aab2c474
Alex <alexesteveshq@gmail.com>
2024-05-28 01:46:50 +0200

    [Alex] test commit manifest

--- pos_order_delete/models/pos_order.py
+++ pos_order_delete/models/pos_order.py
@@ -11,7 +11,8 @@ class PosOrder(models.Model):
         if not self.env.user.has_group('pos_order_delete.group_delete_pos_order'):
             return super()._unlink_except_draft_or_cancel()
         else:
-            for line in self.lines:
-                line.qty = line.qty * -1
-            self._create_order_picking()
-            self.payment_ids.unlink()
+            for order in self:
+                for line in order.lines:
+                    line.qty = line.qty * -1
+                order._create_order_picking()
+                order.payment_ids.unlink()

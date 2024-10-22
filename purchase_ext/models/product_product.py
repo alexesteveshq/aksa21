687fd5867ff106c44c99a874cc8d82feaea96657
alex <alexesteveshq@gmail.com>
2024-10-22 23:33:15 +0200

    [Alex] add import qty

--- purchase_ext/models/product_product.py
+++ purchase_ext/models/product_product.py
@@ -6,9 +6,11 @@ from odoo import fields, models, api, _
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
+    import_qty = fields.Integer(string='Import Qty')
+
     def purchase_from_products(self):
         partner = self.env.ref('__custom__.aksa_partner')
         if self:
-            self.env['purchase.order'].create(
-                {'name': _('Aksa Products'), 'partner_id': partner.id, 'order_line':
-                    [(0, 0, {'product_id': prod.id, 'price_unit': prod.standard_price}) for prod in self]})
+            self.env['purchase.order'].create( {'name': _('Aksa Products'), 'partner_id': partner.id,
+                 'order_line': [(0, 0, {'product_id': prod.id, 'price_unit': prod.standard_price,
+                                        'product_qty': prod.import_qty}) for prod in self]})

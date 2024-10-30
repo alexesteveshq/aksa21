85187d5e9861c46dc1b2711f84a47e4ddd68513d
alex <alexesteveshq@gmail.com>
2024-10-31 00:52:33 +0100

    [Alex] corregir calculo de % de descuento

--- sales_dashboard/models/sales_dashboard.py
+++ sales_dashboard/models/sales_dashboard.py
@@ -31,14 +31,16 @@ class PosOrder(models.Model):
             ('date_order', '>=', month_start_utc),
             ('date_order', '<=', today_utc),
             ('state', 'in', ['paid', 'done', 'invoiced'])
-        ])
+        ]).filtered(lambda o: not o.is_refunded and not o.refunded_orders_count)
 
         # Fetch the previous month's orders up to the same day
         previous_month_orders = self.with_context(active_test=False).sudo().search([
             ('date_order', '>=', previous_month_start_utc),
             ('date_order', '<=', previous_month_end_utc),
+            ('is_refunded', '=', False),
+            ('refunded_orders_count', '=', 0),
             ('state', 'in', ['paid', 'done', 'invoiced'])
-        ])
+        ]).filtered(lambda o: not o.is_refunded and not o.refunded_orders_count)
 
         # Calculate total sales for both periods
         current_month_total_sales = sum(order.amount_currency for order in current_month_orders)
